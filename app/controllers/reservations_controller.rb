41e71fa14320b641f892bc6e7f058ceb4fd22ca1
Adam DePaolo <amdepaolo@gmail.com>
2023-08-14 23:17:44 -0400

    creates reservation controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,21 @@
+class ReservationsController < ApplicationController
+
+    def index
+        pass = MuseumPass.find(params[:museum_pass_id])
+        reservations = pass.reservations
+        render json: reservations
+    end
+
+    def create
+        pass = MuseumPass.find(params[:museum_pass_id])
+        reservation = pass.reservations.create(reservation_params)
+        reservation.expected_check_in = reservation.check_out + 2
+        render json: reservation, status: :created
+    end
+
+    private
+
+    def reservation_params
+        params.permit(:patron_name, :email, :phone, :extra_notes, :patron_card. :check_out)
+    end
+end
